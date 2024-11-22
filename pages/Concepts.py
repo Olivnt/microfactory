2044ea499812aa78d3937abc500e12a52433eef6
Olivnt <olivnt@yahoo.co.uk>
2024-11-22 22:51:15 +0100

    Update Concepts.py

--- pages/Concepts.py
+++ pages/Concepts.py
@@ -5,7 +5,7 @@ st.title("Concept Showcase")
 
 # Concept 1
 st.header("Concept 1: Modular and Flexible Design")
-st.image("Pictures/Concept1.png", caption="Concept 1 Image", se_container_width=True)
+st.image("Pictures/Concept1.png", caption="Concept 1 Image", use_container_width=True)
 st.markdown("""
 **Description:**
 In this solution, wheels are mounted directly on all machines in the production facility. The wheels allow employees to move the machines freely around the production area without the need for additional equipment. The wheels will be specifically selected to ensure durability and stability, so the machines can be moved easily without the risk of tipping or damage.
@@ -19,7 +19,7 @@ In this solution, wheels are mounted directly on all machines in the production 
 
 # Concept 2
 st.header("Concept 2: User-Friendly and Automated Design")
-st.image("Pictures/Concept2.png", caption="Concept 2 Image", se_container_width=True)
+st.image("Pictures/Concept2.png", caption="Concept 2 Image", use_container_width=True)
 st.markdown("""
 **Description:**
 This option involves a custom-made, stable structure with wheels that can hold two or three smaller machines stacked on top of each other. The structure will ensure that the machines remain stable and secure during transport and use. This solution saves space by stacking the machines while maintaining mobility. The structure can also be easily moved and used in areas where space is limited, making it particularly advantageous for educational environments or smaller production areas.
